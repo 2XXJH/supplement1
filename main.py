6f2f9635c427e1a445dfd1f4f414635df096ef8a
JH2XX <158313400+JH2XX@users.noreply.github.com>
2024-11-21 10:30:50 -0500

    doc: added documentation for add

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-def add(num1, num2): 
+def add(num1, num2):
+    ''' Adds two numbers and returns the result.
+
+    Args:
+        num1: the first argument.
+        num2: the second argument.
+
+    Returns:
+        The sum of the two numbers.
+    '''
     return num1 + num2
-
-
 
 def test_should_return_four_for_two_and_two():
     assert add(2, 2) == 4
